e08f3ff349f600f5a2ff2834c6ed06a4ca084a63
LucasMatiasJuarez <lumas89@gmail.com>
2025-09-02 14:42:38 -0300

    seccion var and const

---
+++ hola.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+// Declaracion de constantes
+const Pi float32 = 3.14
+const (
+	x = 100
+	y = 0b100
+	z = 0o12
+	w = 0xFF
+)
+
+const (
+	Domingo = iota + 1
+	Lunes
+	Martes
+	Miercoles
+	Jueves
+	Viernes
+	Sabado
+)
+
+func main() {
+	fmt.Println("Hola, Mundo")
+	fmt.Println(quote.Go())
+	fmt.Println("El valor de Pi es:", Pi)
+	fmt.Println(x, y, z, w)
+	fmt.Println(Domingo, Lunes, Martes, Miercoles, Jueves, Viernes, Sabado)
+	//Declaracion de variables 1ERA FORMA
+	//var firstName string = "Luis"
+	//var age int = 23
+
+	//Declaracion de las variables segunda forma
+
+	var firstName, age = "Luis", 23
+
+	//Declaracion de las variables tercera forma
+	lastName, country, year := "Juarez", "Argentina", 2024
+
+	fmt.Println("Nombre:", firstName)
+	fmt.Println("Edad:", age)
+	fmt.Println("Apellido:", lastName)
+	fmt.Println("Pais:", country)
+	fmt.Println("Año:", year)
+}
